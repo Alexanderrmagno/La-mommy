3c89d3fd6fb76d2ff52c5ba2f7db062f0a0e49ee
Alexandrrmagno <22192@portalcedi.edu.mx>
2024-05-29 13:31:35 -0600

    bay bay

--- Heroe.hpp
+++ Heroe.hpp
@@ -6,54 +6,5 @@
 
 using namespace std;
 
-int bazinga() {
-    sf::RenderWindow window(sf::VideoMode(800, 800), "Movimiento de personaje");
 
-    sf::Texture texture;
-    if (!texture.loadFromFile("boptoronja.png")) {
-        std::cerr << "Error al cargar la imagen character.png" << std::endl;
-        return -1;
-    }
-
-    // Crear el sprite y asignarle la textura
-    sf::Sprite character(texture);
-    character.setPosition(400.f, 400.f);
-
-    // velocidad
-    const float speed = 200.f;
-
-    sf::Clock clock;
-
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        float deltaTime = clock.restart().asSeconds();
-
-        sf::Vector2f movement(0.f, 0.f);
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-            movement.y -= speed * deltaTime;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-            movement.y += speed * deltaTime;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-            movement.x -= speed * deltaTime;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-            movement.x += speed * deltaTime;
-        }
-
-        character.move(movement);
-
-        window.clear();
-        window.draw(character);
-        window.display();
-    }
-
-    return 0;
-}
 #endif
