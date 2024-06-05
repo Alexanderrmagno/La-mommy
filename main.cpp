bc1724fec16bf21f999e2a1a645cc670613aa064
Alexandrrmagno <22192@portalcedi.edu.mx>
2024-06-05 11:50:46 -0600

    barravida

--- main.cpp
+++ main.cpp
@@ -10,20 +10,17 @@
 using namespace sf;
 using namespace std;
 
-vector<Bullet>bullets;
+vector<Bullet> bullets;
 
-enum GameState
-{
+enum GameState {
     MAIN_MENU,
     GAME_RUNNING,
     GAME_OVER
 };
 
-int main()
-{
+int main() {
     SoundBuffer buffer;
-    if (!buffer.loadFromFile("Fondos/anani.wav"))
-    {
+    if (!buffer.loadFromFile("Fondos/anani.wav")) {
         // Error handling...
     }
     Sound sound;
@@ -36,8 +33,7 @@ int main()
     MainMenu menu(window.getSize().x, window.getSize().y);
 
     Texture menuBackgroundTexture;
-    if (!menuBackgroundTexture.loadFromFile("fondos/neoPortada.png"))
-    {
+    if (!menuBackgroundTexture.loadFromFile("fondos/neoPortada.png")) {
         return -1;
     }
     menu.setBackground(menuBackgroundTexture);
@@ -56,32 +52,20 @@ int main()
         !trainTexture.loadFromFile("fondos/gohancomoquedolamoto.png") ||
         !characterTexture.loadFromFile("fondos/Izquierda.png") ||
         !playerHurtTexture.loadFromFile("fondos/JugadorHerido.png") ||
-        !gameOverTexture.loadFromFile("fondos/GameOver.png"))
-    {
+        !gameOverTexture.loadFromFile("fondos/GameOver.png")) {
         return -1;
     }
 
     // Cargar texturas de la barra de vida
     vector<Texture> barraVidaTextures(16);
-    for (int i = 0; i <= 15; ++i)
-    {
-        if (!barraVidaTextures[i].loadFromFile("fondos/BarraVida.png" + to_string(i) + ".png"))
-        {
+    for (int i = 0; i <= 15; ++i) {
+        if (!barraVidaTextures[i].loadFromFile("fondos/BarraVida" + to_string(i) + ".png")) {
             return -1;
         }
     }
 
-    Texture barraVidaTexture1, barraVidaTexture2, barraVidaTexture3; // Definir tantas texturas como necesites
-    if (!barraVidaTexture1.loadFromFile("barra_vida/vida_1.png") ||
-        !barraVidaTexture2.loadFromFile("barra_vida/vida_2.png") ||
-        !barraVidaTexture3.loadFromFile("barra_vida/vida_3.png"))
-    {
-        return -1;
-    }
-
     Texture enemyTexture1, enemyTexture2;
-    if (!enemyTexture1.loadFromFile("fondos/Momia1-1.png") || !enemyTexture2.loadFromFile("fondos/Momia1-2.png"))
-    {
+    if (!enemyTexture1.loadFromFile("fondos/Momia1-1.png") || !enemyTexture2.loadFromFile("fondos/Momia1-2.png")) {
         return -1;
     }
     vector<Texture> enemyTextures = {enemyTexture1, enemyTexture2};
@@ -103,37 +87,27 @@ int main()
 
     Clock clock;
 
-    while (window.isOpen())
-    {
+    while (window.isOpen()) {
         float deltaTime = clock.restart().asSeconds();
 
         Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
             if (event.type == Event::Closed)
                 window.close();
 
-            if (state == MAIN_MENU)
-            {
-                if (event.type == Event::KeyPressed)
-                {
-                    if (event.key.code == Keyboard::Up)
-                    {
+            if (state == MAIN_MENU) {
+                if (event.type == Event::KeyPressed) {
+                    if (event.key.code == Keyboard::Up) {
                         menu.moveUp();
                     }
-                    if (event.key.code == Keyboard::Down)
-                    {
+                    if (event.key.code == Keyboard::Down) {
                         menu.moveDown();
                     }
-                    if (event.key.code == Keyboard::Return)
-                    {
+                    if (event.key.code == Keyboard::Return) {
                         int selectedItem = menu.getPressedItem();
-                        if (selectedItem == 0)
-                        {
+                        if (selectedItem == 0) {
                             state = GAME_RUNNING;
-                        }
-                        else if (selectedItem == 1)
-                        {
+                        } else if (selectedItem == 1) {
                             window.close();
                         }
                     }
@@ -143,12 +117,9 @@ int main()
 
         window.clear();
 
-        if (state == MAIN_MENU)
-        {
+        if (state == MAIN_MENU) {
             menu.draw(window);
-        }
-        else if (state == GAME_RUNNING)
-        {
+        } else if (state == GAME_RUNNING) {
             sound.stop();
             player.handleInput();
             player.applyGravity();
@@ -164,28 +135,23 @@ int main()
             window.draw(enemy1.sprite);
             window.draw(enemy1.barraVidaSprite); // Dibuja la barra de vida
 
-            if (Keyboard::isKeyPressed(Keyboard::E))
-            {
-                float xPlayer = player.sprite.getGlobalBounds().getPosition().x;
-                float yPlayer = player.sprite.getGlobalBounds().top +20;
-                Bullet gun(Vector2f(xPlayer,yPlayer), player.direction);
+            if (Keyboard::isKeyPressed(Keyboard::E)) {
+                float xPlayer = player.sprite.getPosition().x;
+                float yPlayer = player.sprite.getPosition().y + 20;
+                Bullet gun(Vector2f(xPlayer, yPlayer), player.direction);
                 bullets.push_back(gun);
             }
 
-
-        }
-        for(auto &r : bullets)
-        {
+            for (auto& r : bullets) {
                 r.update(player);
                 r.drawTo(window);
-            if(r.shape.getGlobalBounds().getPosition().x == enemy1.sprite.getGlobalBounds().getPosition().x || 
-            r.shape.getGlobalBounds().getPosition().y == enemy1.sprite.getGlobalBounds().getPosition().y)
-            {
-                enemy1.alive = 0;
-            }
-            if(r.shape.getGlobalBounds().getPosition().x == 600 || r.shape.getGlobalBounds().getPosition().x  == 0)
-            {
-                r.vel = 0;
+                if (r.shape.getPosition().x == enemy1.sprite.getPosition().x ||
+                    r.shape.getPosition().y == enemy1.sprite.getPosition().y) {
+                    enemy1.alive = 0;
+                }
+                if (r.shape.getPosition().x == 600 || r.shape.getPosition().x == 0) {
+                    r.vel = 0;
+                }
             }
         }
 
